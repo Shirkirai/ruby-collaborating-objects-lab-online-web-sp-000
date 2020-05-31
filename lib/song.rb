60c78499eeaf61cd3734aed8b35ffac22eefc126
Jung Lee <lee.jung.s95@gmail.com>
2020-05-31 23:01:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 require 'pry'
+
 class Song
   attr_accessor :name
 
@@ -7,7 +8,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    
+
     song = self.new
     song.title = filename.split(" - ")[1]
 
